245852454e8160508690d98ee236a0456fe4884c
邹铭杰 <zoumingjie17@163.com>
2023-10-18 14:18:40 +0800

    feat:Workaround for compileCommonMainKotlinMetadata task failed

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,14 @@ plugins {
     id("org.jetbrains.dokka")
     id("com.vanniktech.maven.publish") version "0.25.3" apply false
 }
+
+/**
+ * This is a workaround for https://github.com/JetBrains/compose-multiplatform/issues/3428
+ */
+subprojects {
+    configurations.all {
+        resolutionStrategy {
+            force("org.jetbrains.kotlinx:atomicfu:0.21.0")
+        }
+    }
+}
