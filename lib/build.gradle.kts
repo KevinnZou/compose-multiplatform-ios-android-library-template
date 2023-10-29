6e7abab505be0c67b9e68d0f81f7ad0cb4003b1b
邹铭杰 <zoumingjie17@163.com>
2023-10-29 20:40:01 +0800

    feat:Ktlint Format

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,14 +9,14 @@ plugins {
 }
 
 kotlin {
-    androidTarget() {
+    androidTarget {
         publishLibraryVariants("release")
     }
 
     listOf(
         iosX64(),
         iosArm64(),
-        iosSimulatorArm64()
+        iosSimulatorArm64(),
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "shared"
